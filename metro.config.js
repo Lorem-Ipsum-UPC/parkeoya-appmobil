da70126c159386c821f1579700fefcc103629cca
Jhinta <raisotil7@hotmail.com>
2025-12-03 23:59:08 -0500

    feat: optimizacion

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
  
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
+
+// Optimizaciones para reducir tiempo de build
+config.transformer = {
+  ...config.transformer,
+  minifierPath: 'metro-minify-terser',
+  minifierConfig: {
+    compress: {
+      drop_console: false, // Mantener console.log en desarrollo
+    },
+  },
+};
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'css'],
+};
  
-module.exports = withNativeWind(config, { input: './global.css' })
+module.exports = withNativeWind(config, { input: './global.css' });
